0c2b2c46610d886d01834fab149b1c8f647ed55d
Chris Mlinac <chrismlinac@gmail.com>
2019-11-01 14:38:39 -0500

    Updates

---
+++ precache-manifest.8dc7ae106a4d27ff03a54cda614e179f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249c93248f1b852668249f2356af5b5b",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "a7827b868809a132e3a8",
+    "url": "/MemoryGame/static/css/main.02edf9a1.chunk.css"
+  },
+  {
+    "revision": "dfbc7d1f88bbb7beae3d",
+    "url": "/MemoryGame/static/js/2.4f20b97c.chunk.js"
+  },
+  {
+    "revision": "a7827b868809a132e3a8",
+    "url": "/MemoryGame/static/js/main.12f88054.chunk.js"
+  },
+  {
+    "revision": "8b397585f1b887bce1fe",
+    "url": "/MemoryGame/static/js/runtime-main.cf76606b.js"
+  }
+]);
